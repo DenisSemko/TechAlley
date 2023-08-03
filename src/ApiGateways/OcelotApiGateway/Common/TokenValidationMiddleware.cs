f48867578a6e0a1f141cb01cf9be2891727ff93b
Denis Semko <53062219+DenisSemko@users.noreply.github.com>
2023-08-03 17:02:22 +0300

    Implement Access/Refresh Token validation

--- src/ApiGateways/OcelotApiGateway/Common/TokenValidationMiddleware.cs
+++ src/ApiGateways/OcelotApiGateway/Common/TokenValidationMiddleware.cs
@@ -14,36 +14,52 @@ public class TokenValidationMiddleware
     public async Task Invoke(HttpContext context)
     {
         if (context.Request.Path.Value.Contains(Constants.AuthRequestPaths.LoginPath) ||
-            context.Request.Path.Value.Contains(Constants.AuthRequestPaths.RegistrationPath) ||
-            context.Request.Path.Value.Contains(Constants.AuthRequestPaths.CatalogPath))
+            context.Request.Path.Value.Contains(Constants.AuthRequestPaths.RegistrationPath))
         {
             await _next(context);
         }
         else
         {
             context.Request.Headers.Add("Authorization", context.Request.Cookies["Authorization"]);
-            string? token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            string? accessToken = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            string? refreshToken = context.Request.Cookies["Refresh"];
 
-            if (token is not null)
+            if (accessToken is not null)
             {
                 try
                 {
-                    JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-                    byte[] key = Encoding.ASCII.GetBytes(_configuration["JWT:Secret"]);
-                
-                    tokenHandler.ValidateToken(token, new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(key),
-                        ValidateIssuer = true,
-                        ValidIssuer = _configuration["JWT:Issuer"],
-                        ValidateAudience = true,
-                        ValidAudience = _configuration["JWT:Audience"],
-                        ValidateLifetime = true,
-                        ClockSkew = TimeSpan.Zero
-                    }, out var validatedToken);
-                    
+                    JwtSecurityTokenHandler tokenHandler = new();
+                    tokenHandler.ValidateToken(accessToken, CreateTokenValidationParams(), out var validatedToken);
                     context.Items["ValidatedToken"] = validatedToken;
+                }
+                catch (SecurityTokenExpiredException)
+                {
+                    try
+                    {
+                        JwtSecurityTokenHandler tokenHandler = new();
+                        HttpClient httpClient = new ();
+                        tokenHandler.ValidateToken(refreshToken, CreateTokenValidationParams(), out var validatedToken);
+                        
+                        HttpResponseMessage response = await httpClient.GetAsync(string.Format(Constants.AuthRequestPaths.RefreshPath, accessToken));
+                        string content = await response.Content.ReadAsStringAsync();
+                        TokenModel model = JsonConvert.DeserializeObject<TokenModel>(content);
+
+                        context.Request.Headers["Authorization"] = model.AccessToken;
+                        context.Response.Cookies.Append("Authorization", model.AccessToken, new CookieOptions
+                        {
+                           HttpOnly = true
+                        });
+                        context.Response.Cookies.Append("Refresh", model.RefreshToken, new CookieOptions
+                        {
+                           HttpOnly = true
+                        });
+
+                    }
+                    catch (Exception)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                        return;
+                    }
                 }
                 catch (Exception)
                 {
@@ -59,4 +75,22 @@ public class TokenValidationMiddleware
             await _next(context); 
         }
     }
+
+    private TokenValidationParameters CreateTokenValidationParams()
+    {
+        byte[] key = Encoding.ASCII.GetBytes(_configuration["JWT:Secret"]);
+        TokenValidationParameters tokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = true,
+            ValidIssuer = _configuration["JWT:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = _configuration["JWT:Audience"],
+            ValidateLifetime = true,
+            ClockSkew = TimeSpan.Zero
+        };
+
+        return tokenValidationParameters;
+    } 
 }
